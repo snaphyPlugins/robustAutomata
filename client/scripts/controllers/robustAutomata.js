f0312b599962ddfc109ca5da27014b5dc81e2448
robinskumar73 <robinskumar73@gmail.com>
2016-02-20 19:44:30 +0530

    First commit

---
+++ client/scripts/controllers/robustAutomata.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module($snaphy.getModuleName())
+
+//Controller for robustAutomataControl ..
+.controller('robustAutomataControl', ['$scope', '$stateParams', 'Database', 'Resource',
+    function($scope, $stateParams, Database, Resource) {
+        //Checking if default templating feature is enabled..
+        var defaultTemplate = $snaphy.loadSettings('robustAutomata', "defaultTemplate");
+        $snaphy.setDefaultTemplate(defaultTemplate);
+        //Use Database.getDb(pluginName, PluginDatabaseName) to get the Database Resource.
+        //$scope.tableState =
+        var ctrl = $scope;
+
+        $scope.displayed = [];
+
+        $scope.callServer = function callServer(tableState, ctrl) {
+
+            if (!$scope.stCtrl && ctrl) {
+                $scope.stCtrl = ctrl;
+            }
+
+            if (!tableState && $scope.stCtrl) {
+                $scope.stCtrl.pipe();
+                return;
+            }
+
+            $scope.isLoading = true;
+
+            var pagination = tableState.pagination;
+
+            var start = pagination.start || 0; // This is NOT the page number, but the index of item in the list that you want to use to display the table.
+            var number = pagination.number || 10; // Number of entries showed per page.
+
+            Resource.getPage(start, number, tableState).then(function(result) {
+
+                $scope.displayed = result.data;
+                tableState.pagination.numberOfPages = result.numberOfPages; //set the number of pages so the pagination can update
+                $scope.isLoading = false;
+            });
+        };
+
+    } //controller function..
+]);
